e54fa558df63b0133d5ba61c5276b6de3ce2e591
Stephyj2 <stephyjohn398@gmail.com>
2019-06-25 10:22:32 +0530

    Add files via upload
    Iot based Smart Room

---
+++ LDR_Ubidots.ino
@@ -0,0 +1,28 @@
+#include "UbidotsMicroESP8266.h"
+//#define TOKEN  "A1E-4Y75tlzDNAvdIcLd63qwZkLNhDK2eP"  // Put here your Ubidots TOKEN
+#define TOKEN  ""
+#define WIFISSID ""
+#define PASSWORD ""
+
+Ubidots client(TOKEN);
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+  client.wifiConnection(WIFISSID, PASSWORD);
+}
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  delay(1000);
+   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  
+  // print out the value you read:
+  Serial.println(voltage);
+  delay(1000);
+    client.add("LDR Voltage", voltage);
+    client.sendAll(true);
+}
---
+++ newmupload.ino.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+#include "ThingSpeak.h"
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+
+//Thingspeak Channel Field Values Measurement
+unsigned long myChannelNumber = 534628;
+const char * myWriteAPIKey = "MC2KU41IN5H3MV78";
+
+#define WIFI_SSID "INTERNSHIP"
+#define WIFI_PASSWORD "ictak@Guest"
+#include <DHT.h>
+#include <DHT_U.h>
+#include<dht.h>
+dht DHT;
+#define DHT11_D0
+WiFiClient client; 
+     void setup(){
+                    Serial.begin(9600);
+                    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);// connect to wifi.
+                    ThingSpeak.begin(client);
+                    Serial.print("connecting");
+                    while (WiFi.status() != WL_CONNECTED) {
+                    Serial.print(".");
+                    delay(500);
+                 }
+Serial.println();
+Serial.print("connected: ");
+Serial.println(WiFi.localIP());
+void loop()
+      {
+         int chk = DHT.read11(DHT11_PIN);
+         Serial.print("Temperature = ");
+         Serial.println(DHT.temperature);
+         Serial.print("Humidity = ");
+         Serial.println(DHT.humidity);
+         delay(1000);
+         ThingSpeak.setField(1,cm);
+         ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+         delay(20000);
+      }
